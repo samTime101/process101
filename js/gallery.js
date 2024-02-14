9653c2248d5c22d266db550be8e00b182e2c99db
samTime101 <samipregmi44@gmail.com>
2024-02-14 12:26:58 +0545

    gallery file fixes

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,25 @@
-document.getElementById('next')
-    .onclick = function () {
-        let lists = document.querySelectorAll('.item');
-        document.getElementById('slide')
-            .appendChild(lists[0]);
-    }
-document.getElementById('prev')
-    .onclick = function () {
-        let lists = document.querySelectorAll('.item');
-        document.getElementById('slide')
-            .prepend(lists[lists.length - 1]);
-    }
+const items = document.querySelectorAll('.item');
+let currentItemIndex = 0;
+
+function showItem(index) {
+    items.forEach((item, i) => {
+        if (i === index) {
+            item.style.display = 'block';
+        } else {
+            // HIDING OTHER IMAGE
+            item.style.display = 'none';
+        }
+    });
+}
+
+showItem(currentItemIndex);
+
+document.getElementById('prev').addEventListener('click', function () {
+    currentItemIndex = (currentItemIndex - 1 + items.length) % items.length;
+    showItem(currentItemIndex);
+});
+
+document.getElementById('next').addEventListener('click', function () {
+    currentItemIndex = (currentItemIndex + 1) % items.length;
+    showItem(currentItemIndex);
+});
