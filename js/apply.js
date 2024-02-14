8e7b13a116e54a4e1e3891e6f0b7dc3e6fed60ff
samTime101 <samipregmi44@gmail.com>
2024-02-14 08:31:22 +0545

    some minor adjustments

--- js/apply.js
+++ js/apply.js
@@ -1,7 +1,9 @@
-// https://stackoverflow.com/questions/27600506/stop-page-from-redirecting-after-submitting-google-form-through-ajax
+// // https://stackoverflow.com/questions/27600506/stop-page-from-redirecting-after-submitting-google-form-through-ajax
 $(document).ready(function () {
     $('#form').submit(function (e) {
         e.preventDefault();
+        $('#submit-btn').prop('disabled', true);
+        $('#submit-btn').css({'background-color': 'green','color': 'white'}).val('Submitting...');
         $.ajax({
             url: "https://script.google.com/macros/s/AKfycbyp1ZF6vaxCE_Fl22vSDQ6GLQzudxSNSg2MtUgt40AZ2RW-z3VppHXSlibh407Ilk4sTg/exec",
             data: $(this).serialize(),
